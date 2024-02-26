cba063bd571f47fe40577b33c73b5708b9e8525d
VniGG <161388238+VniGG@users.noreply.github.com>
2024-02-26 20:13:58 -0300

    Primeiro commit2
    A

--- ProjetoSoma/src/Classeprincipal.java
+++ ProjetoSoma/src/Classeprincipal.java
@@ -2,7 +2,11 @@
 public class Classeprincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int x = 5;
+		int y =7;
+		
+		System.out.println(x + y);
 
 	}
 
